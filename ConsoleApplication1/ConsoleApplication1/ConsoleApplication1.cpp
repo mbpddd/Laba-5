d991c0afa4bad36efdc313f8d443d8583d7486ff
mirosastav02@gmail.com <mirosastav02@gmail.com>
2022-11-27 22:11:39 +0200

    Laba-5

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int v = 4;
+	int * ptrv = &v;
+	
+	cout << "ptrv = " << *ptrv<<endl;
+
+	cout << "enter v here - ";
+
+	cin >> v;
+	cout << "now you change v and ptrv and they are equal - " << v << endl;
+	
+
+}
